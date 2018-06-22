3143085beb604469dbec13a6db56fbf3d323a414
DK <dkdan94@gmail.com>
2018-06-22 12:48:29 -0400

    fetches commit data from github api and logs to the console

--- app/components/Home.js
+++ app/components/Home.js
@@ -8,12 +8,25 @@ type Props = {};
 export default class Home extends Component<Props> {
   props: Props;
 
+  state = {
+    data: []
+  };
+
+  renderCommits = () => {
+    fetch('https://api.github.com/repos/mmcdevi1/git_test/commits')
+      .then(res => res.json())
+      .then(data => this.setState({ data }))
+      .catch(err => console.log(err));
+  };
+
   render() {
+    console.log(this.state.data);
     return (
       <div>
         <div className={styles.container} data-tid="container">
           <h2>Home</h2>
           <Link to="/counter">to Counter</Link>
+          <button onClick={this.renderCommits}>Button</button>
         </div>
       </div>
     );
