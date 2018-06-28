71cc011f91a3f6ddbcace38f5a1af95547da949c
Blake Spencer <blake.spencer1@gmail.com>
2018-06-28 16:28:57 -0400

    rendering out commits in the commits page instead of Aside Navbar

--- app/components/Repos/BranchesPage.js
+++ app/components/Repos/BranchesPage.js
@@ -1,13 +1,122 @@
 import React from 'react';
+import NavItem from '../Nav/Aside/NavItem';
+import { connect } from 'react-redux';
+import styles from '../Nav/Aside/Aside.css';
 
 class BranchesPage extends React.Component {
-	render () {
-		return (
+  state = {
+    selectedBranch: {},
+  };
+  renderCommits() {
+    let isMaster = false;
+    let output = [];
+    const { masterCommits, branchCommits } = this.props;
+
+    const masterShas = masterCommits.map(commit => {
+      return commit.sha;
+    });
+    for (let i = 0; i < branchCommits.length; i++) {
+      if (!masterShas.includes(branchCommits[i].sha)) {
+        output.push(branchCommits[i]);
+      }
+    }
+    if (this.props.selectedBranch.name === 'master') {
+      output = masterCommits;
+      isMaster = true;
+    }
+    if (this.isClosedBranch()) {
+      output = branchCommits;
+    }
+
+    console.log('output', output[0] && output[0].commit.message);
+
+    return (
       <div>
-        Branches
+        {output.map(commit => {
+          return (
+            <NavItem
+              key={commit.sha}
+              path={`/repos/${this.props.selectedRepo}/commits/${commit.sha}`}
+              name={commit.commit.message}
+              isCommit={true}
+              sha={commit.sha}
+            />
+          );
+        })}
+        {isMaster || this.isClosedBranch() ? null : this.renderMasterCommits()}
       </div>
-    )
-	}
+    );
+  }
+
+  renderMasterCommits() {
+    return (
+      <div>
+        <div
+          className={styles.menu}
+          style={{
+            color: 'black',
+          }}
+        >
+          Master
+        </div>
+        {this.props.masterCommits.map(commit => {
+          return (
+            <NavItem
+              key={commit.sha}
+              path={`/repos/${this.props.selectedRepo}/commits/${commit.sha}`}
+              name={commit.commit.message}
+              isCommit={true}
+              sha={commit.sha}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+
+  isClosedBranch() {
+    for (let i = 0; i < this.props.closedBranches.length; i++) {
+      if (
+        this.props.selectedBranch.head &&
+        this.props.selectedBranch.head.ref ===
+          this.props.closedBranches[i].head.ref
+      ) {
+        return true;
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div>Branches</div>
+        {this.props.branchCommits ? (
+          <div className={styles.menu_group}>
+            <div className={styles.menu}>Commits</div>
+            <div
+              style={{
+                overflow: 'scroll',
+                height: '300px',
+              }}
+            >
+              {this.renderCommits()}
+            </div>
+          </div>
+        ) : null}
+      </div>
+    );
+  }
 }
 
-export default BranchesPage;
+const mapStateToProps = state => ({
+  branchCommits: state.branchCommits,
+  masterCommits: state.masterCommits,
+  selectedBranch: state.selectedBranch,
+  selectedRepo: state.selectedRepo,
+  closedBranches: state.closedBranches,
+});
+
+export default connect(
+  mapStateToProps,
+  null
+)(BranchesPage);
