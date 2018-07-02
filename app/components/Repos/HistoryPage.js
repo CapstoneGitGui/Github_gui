77f608118732748955d2dc866659f5c7fc283e2a
Marino Demo <demo716@gmail.com>
2018-07-02 14:35:30 -0400

    able to history of commits

--- app/components/Repos/HistoryPage.js
+++ app/components/Repos/HistoryPage.js
@@ -1,13 +1,62 @@
 import React from 'react';
+import {connect} from 'react-redux'
+import Column from '../UI/Column'
+import ContentWrapper from '../UI/ContentWrapper';
+import Header from '../UI/Header';
+import CommitsList from '../Commits/CommitsList'
+import SelectedCommit from '../Commits/SelectedCommit'
 
 class HistoryPage extends React.Component {
-	render () {
-		return (
-      <div>
-        history
-      </div>
-    )
-	}
+  // constructor(props) {
+  //   super(props) {
+  //     this.state = {
+
+  //     }
+  //   }
+  // }
+
+
+  // componentDidMount () {
+
+  // }
+
+
+  renderCommit () {
+    const {selectedCommit} = this.props
+
+    if (Object.keys(selectedCommit).length) {
+      return <SelectedCommit commit={selectedCommit} />
+    }
+  }
+
+	render() {
+    const {
+      match: { params },
+      selectedCommit,
+    } = this.props
+
+    return (
+      <ContentWrapper>
+        <Column className='right'>
+          <Header>
+            {params.id}
+          </Header>
+          <CommitsList />
+        </Column>
+        <Column className='left'>
+          { this.renderCommit() }
+        </Column>
+      </ContentWrapper>
+    );
+  }
 }
 
-export default HistoryPage;
+function mapStateToProps (state) {
+  return {
+    selectedCommit: state.selectedCommit,
+    selectedRepoCommits: state.masterCommits
+  }
+}
+
+export default connect(mapStateToProps)(HistoryPage)
+
